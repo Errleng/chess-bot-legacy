d1f1b1142fe543bb207aec73dc8d65232040a4c7
ELEVATE <dullbland@gmail.com>
2018-03-03 22:46:49 -0500

    Support for both sides of the board (White and Black)

--- VersionPy/CleanedUp.py
+++ VersionPy/CleanedUp.py
@@ -37,13 +37,16 @@ def main():
 
     LAPTOP = False
 
-    move_only = False
+    move_only = True
     new_board = False
 
     IMAGES_PATH, TEMPLATES_PATH, TIMERS_PATH, ENGINE_PATH = "", "", "", ""
 
     BOARD_DIM, PIECE_DIM, TIMER_RESIZE_WIDTH, BOARD_RESIZE_WIDTH, TIMER_SIMILARITY_THRESHOLD, PIECE_SIMILARITY_THRESHOLD = 0, 0, 0, 0, 0, 0
     BLACK_TIMER_BBOX, WHITE_TIMER_BBOX, BOARD_BBOX = 0, 0, 0
+    
+    BLACK = "black"
+    WHITE = "white"
 
     if LAPTOP:
         IMAGES_PATH = "C:/Users/aisae/Documents/Sourcetree/ChessBot/Screenshots/Laptop/"
@@ -90,7 +93,8 @@ def main():
 
     FILE_TYPE = ".png"
 
-    SQUARE_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8]
+    WHITE_SQUARE_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8]
+    BLACK_SQUARE_NUMBERS = [8, 7, 6, 5, 4, 3, 2, 1]
 
     SLEEP_DELAY = 0.1
 
@@ -142,19 +146,28 @@ def main():
     white_kingside, white_queenside, black_kingside, black_queenside = True, True, True, True
 
     if not new_board:
-        player = input("Enter the player's side: ")
+        player = input("Enter the player's side: ").lower()
 
         move_count = int(input("Enter the number moves so far in the game: ")) # How many moves have passed in the game
         speed_mode = 0 # How fast and accurate the program is
-        difficulty = 0 # How strong the program plays
+        difficulty = 1 # How strong the program plays
 
     if not new_board and difficulty == 0:
         if ENGINE_NAME == "Rybkav2.3.2a.mp.x64.exe":
-            elo = 1500
+            # elo = 1500
             # chess_engine.setoption({"UCI_LimitStrength": True})
             # chess_engine.setoption({"UCI_Elo": elo})
             chess_engine.setoption({"MultiPV": 3})
-            print("Set Rybka's ELO to", elo, "and set MultiPV to 3")
+            print("Set Rybka's MultiPV to 3")
+        elif ENGINE_NAME == "stockfish_9_x64.exe":
+            chess_engine.setoption({"MultiPV": 3})
+            print("Set Stockfish's MultiPV to 3")
+    elif not new_board and difficulty == 1:
+        if ENGINE_NAME == "Rybkav2.3.2a.mp.x64.exe":
+            elo = 1700
+            chess_engine.setoption({"UCI_LimitStrength": True})
+            chess_engine.setoption({"UCI_Elo": elo})
+            print("Set Rybka's ELO to", elo)
         elif ENGINE_NAME == "stockfish_9_x64.exe":
             chess_engine.setoption({"MultiPV": 3})
             print("Set Stockfish's MultiPV to 3")
@@ -169,8 +182,8 @@ def main():
         image_count = 0
         template_name_count = 0
 
-        for row in SQUARE_NUMBERS:
-            for col in SQUARE_NUMBERS:
+        for row in WHITE_SQUARE_NUMBERS:
+            for col in WHITE_SQUARE_NUMBERS:
                 square_image = resized_board_capture[int(RESIZED_PIECE_DIM * (row-1)): int(RESIZED_PIECE_DIM * row), int(RESIZED_PIECE_DIM * (col-1)): int(RESIZED_PIECE_DIM * col)]
                 image_count += 1
 
@@ -188,6 +201,12 @@ def main():
 
             black_timer_capture = numpy.array(ImageGrab.grab(BLACK_TIMER_BBOX))
             white_timer_capture = numpy.array(ImageGrab.grab(WHITE_TIMER_BBOX))
+
+            if player == BLACK:
+                black_timer_capture, white_timer_capture = white_timer_capture, black_timer_capture
+
+            # cv2.imshow("black timer", black_timer_capture)
+            # cv2.waitKey(1000)
 
             # Image.fromarray(black_timer_capture, "RGB").save(TIMERS_PATH + "BlackTimer.png")
             # Image.fromarray(white_timer_capture, "RGB").save(TIMERS_PATH + "WhiteTimer.png")
@@ -223,17 +242,19 @@ def main():
                 else:
                     moving_side = "w"
             else:
-                moving_side = "w"
+                moving_side = player[0]
                 print("Timer similarities are the same!")
 
-            # print("Moving side:", moving_side)
+            # print("Moving side:", moving_side, black_timer_similarity, white_timer_similarity)
 
-            if moving_side == "b" and player.lower() == "white":
+            if moving_side == "b" and player == WHITE:
+                time.sleep(SLEEP_DELAY)
                 continue
-            if moving_side == "w" and player.lower() == "black":
+            if moving_side == "w" and player == BLACK:
+                time.sleep(SLEEP_DELAY)
                 continue
 
-            time.sleep(SLEEP_DELAY)
+            time.sleep(SLEEP_DELAY * 5)
 
             board_capture = numpy.array(ImageGrab.grab(BOARD_BBOX), dtype = numpy.uint8)
             board_capture = cv2.cvtColor(board_capture, cv2.COLOR_BGR2RGB)
@@ -249,53 +270,96 @@ def main():
             # print("Time to determine moving side:", start_template_time - start_time)
 
             # Start board template match
-            for row in SQUARE_NUMBERS:
-                for col in SQUARE_NUMBERS:
-                    square_image = resized_board_capture[int(RESIZED_PIECE_DIM * (row-1)): int(RESIZED_PIECE_DIM * row), int(RESIZED_PIECE_DIM * (col-1)): int(RESIZED_PIECE_DIM * col)]
+            if player == WHITE:
+                for row in WHITE_SQUARE_NUMBERS:
+                    for col in WHITE_SQUARE_NUMBERS:
+                        square_image = resized_board_capture[int(RESIZED_PIECE_DIM * (row-1)): int(RESIZED_PIECE_DIM * row), int(RESIZED_PIECE_DIM * (col-1)): int(RESIZED_PIECE_DIM * col)]
 
-                    similarity_list = []
-                    for mat in template_mats:
-                        similarity = rgbTemplateMatchMat(square_image, mat)
-                        similarity_list.append(similarity)
+                        similarity_list = []
+                        for mat in template_mats:
+                            similarity = rgbTemplateMatchMat(square_image, mat)
+                            similarity_list.append(similarity)
 
-                    best_match = max(similarity_list)
-                    best_match_index = similarity_list.index(best_match)
+                        best_match = max(similarity_list)
+                        best_match_index = similarity_list.index(best_match)
 
-                    # print(best_match)
-                    # if 0.5 < best_match < 0.9:
-                    #     cv2.imshow("Piece", square_image)
-                    #     cv2.waitKey(0)
+                        # print(best_match)
+                        # if 0.5 < best_match < 0.9:
+                        #     cv2.imshow("Piece", square_image)
+                        #     cv2.waitKey(0)
 
-                    if best_match > PIECE_SIMILARITY_THRESHOLD:
-                        best_template = templates[best_match_index]
-                        if empty_square_count != 0:
-                            FEN += str(empty_square_count)
-                            empty_square_count = 0
+                        if best_match > PIECE_SIMILARITY_THRESHOLD:
+                            best_template = templates[best_match_index]
+                            if empty_square_count != 0:
+                                FEN += str(empty_square_count)
+                                empty_square_count = 0
 
-                        # if best_template[0] == "B":
-                            # FEN += best_template[1].lower()
-                        # else:
-                        #     FEN += best_template[1]
-                        FEN += best_template[1]
+                            # if best_template[0] == "B":
+                                # FEN += best_template[1].lower()
+                            # else:
+                            #     FEN += best_template[1]
+                            FEN += best_template[1]
 
-                        row_slash_count += 1
-                        if row_slash_count >= 8:
-                            row_slash_count -= 8
-                            FEN += "/"
-                    else:
-                        row_slash_count += 1
-                        empty_square_count += 1
-                        if row_slash_count >= 8:
-                            FEN += str(empty_square_count)
-                            FEN += "/"
-                            row_slash_count -= 8
-                            empty_square_count = 0
+                            row_slash_count += 1
+                            if row_slash_count >= 8:
+                                row_slash_count -= 8
+                                FEN += "/"
+                        else:
+                            row_slash_count += 1
+                            empty_square_count += 1
+                            if row_slash_count >= 8:
+                                FEN += str(empty_square_count)
+                                FEN += "/"
+                                row_slash_count -= 8
+                                empty_square_count = 0
+            elif player == BLACK:
+                for row in BLACK_SQUARE_NUMBERS:
+                    for col in BLACK_SQUARE_NUMBERS:
+                        square_image = resized_board_capture[int(RESIZED_PIECE_DIM * (row - 1)): int(RESIZED_PIECE_DIM * row), int(RESIZED_PIECE_DIM * (col - 1)): int(RESIZED_PIECE_DIM * col)]
+
+                        similarity_list = []
+                        for mat in template_mats:
+                            similarity = rgbTemplateMatchMat(square_image, mat)
+                            similarity_list.append(similarity)
+
+                        best_match = max(similarity_list)
+                        best_match_index = similarity_list.index(best_match)
+
+                        # print(best_match)
+                        # if 0.5 < best_match < 0.9:
+                        #     cv2.imshow("Piece", square_image)
+                        #     cv2.waitKey(0)
+
+                        if best_match > PIECE_SIMILARITY_THRESHOLD:
+                            best_template = templates[best_match_index]
+                            if empty_square_count != 0:
+                                FEN += str(empty_square_count)
+                                empty_square_count = 0
+
+                                # if best_template[0] == "B":
+                                # FEN += best_template[1].lower()
+                            # else:
+                            #     FEN += best_template[1]
+                            FEN += best_template[1]
+
+                            row_slash_count += 1
+                            if row_slash_count >= 8:
+                                row_slash_count -= 8
+                                FEN += "/"
+                        else:
+                            row_slash_count += 1
+                            empty_square_count += 1
+                            if row_slash_count >= 8:
+                                FEN += str(empty_square_count)
+                                FEN += "/"
+                                row_slash_count -= 8
+                                empty_square_count = 0
 
             # Finish board template match
 
             end_template_time = time.time()
 
-            print("Time to template match:", end_template_time - start_template_time)
+            # print("Time to template match:", end_template_time - start_template_time)
 
             if FEN.endswith("/"):
                 FEN = FEN[:-1]
@@ -321,11 +385,13 @@ def main():
             board = chess.Board(formatted_FEN)
             chess_engine.position(board)
 
-            print(board)
+            # print(board)
 
-            search_depth = random.randint(4, 7)
+            search_depth = 8
+            if difficulty == 0:
+                search_depth = random.randint(4, 7)
 
-            depth = chess_engine.go(search_depth)
+            depth = chess_engine.go(depth = search_depth)
             best_move = str(depth[0])
 
             if difficulty == 0:
@@ -371,7 +437,31 @@ def main():
 
             # print("Time to calculate move:", end_move_time - end_template_time)
 
-            print("Search Depth:", search_depth)
+            # print("Search Depth:", search_depth)
+
+            if move_only:
+                if player == BLACK:
+                    first_half_move = best_move[:2]
+                    second_half_move = best_move[2:4]
+
+                    first_top = (BOARD_DIM - PIECE_DIM * (9 - int(first_half_move[1]))) + PIECE_DIM // 2
+                    first_left = PIECE_DIM * (7 - (ord(first_half_move[0]) - 97)) + PIECE_DIM // 2
+
+                    second_top = (BOARD_DIM - PIECE_DIM * (9 - int(second_half_move[1]))) + PIECE_DIM // 2
+                    second_left = PIECE_DIM * (7 - (ord(second_half_move[0]) - 97)) + PIECE_DIM // 2
+                else:
+                    first_half_move = best_move[:2]
+                    second_half_move = best_move[2:4]
+
+                    first_top = (BOARD_DIM - PIECE_DIM * int(first_half_move[1])) + PIECE_DIM // 2
+                    first_left = PIECE_DIM * (ord(first_half_move[0]) - 97) + PIECE_DIM // 2
+
+                    second_top = (BOARD_DIM - PIECE_DIM * int(second_half_move[1])) + PIECE_DIM // 2
+                    second_left = PIECE_DIM * (ord(second_half_move[0]) - 97) + PIECE_DIM // 2
+
+                cv2.arrowedLine(board_capture, (first_left, first_top), (second_left, second_top), (255, 0, 0), 5)
+                cv2.imshow("Best move on board", board_capture)
+                cv2.waitKey(2000)
 
             if move_only or best_move == "0000":
                 continue
@@ -392,14 +482,24 @@ def main():
             if 'h8' in best_move:
                 black_kingside = False
 
-            first_half_move = best_move[:2]
-            second_half_move = best_move[2:4]
+            if player == BLACK:
+                first_half_move = best_move[:2]
+                second_half_move = best_move[2:4]
 
-            first_top = 160 + (BOARD_DIM - PIECE_DIM * int(first_half_move[1])) + 50
-            first_left = 285 + PIECE_DIM * (ord(first_half_move[0]) - 97) + 50
+                first_top = 160 + (BOARD_DIM - PIECE_DIM * (9 - int(first_half_move[1]))) + 50
+                first_left = 285 + PIECE_DIM * (7 - (ord(first_half_move[0]) - 97)) + 50
 
-            second_top = 160 + (BOARD_DIM - PIECE_DIM * int(second_half_move[1])) + 50
-            second_left = 285 + PIECE_DIM * (ord(second_half_move[0]) - 97) + 50
+                second_top = 160 + (BOARD_DIM - PIECE_DIM * (9 - int(second_half_move[1]))) + 50
+                second_left = 285 + PIECE_DIM * (7 - (ord(second_half_move[0]) - 97)) + 50
+            else:
+                first_half_move = best_move[:2]
+                second_half_move = best_move[2:4]
+
+                first_top = 160 + (BOARD_DIM - PIECE_DIM * int(first_half_move[1])) + 50
+                first_left = 285 + PIECE_DIM * (ord(first_half_move[0]) - 97) + 50
+
+                second_top = 160 + (BOARD_DIM - PIECE_DIM * int(second_half_move[1])) + 50
+                second_left = 285 + PIECE_DIM * (ord(second_half_move[0]) - 97) + 50
 
             # print(first_left, first_top)
 
@@ -415,7 +515,7 @@ def main():
 
             move_count += 1
 
-            print("Time to loop:", time.time() - start_time)
+            # print("Time to loop:", time.time() - start_time)
 
             time.sleep(SLEEP_DELAY)
 main()
