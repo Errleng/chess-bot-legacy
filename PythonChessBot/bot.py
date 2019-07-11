2e81686aeb19bc0ff21b1c8cc080216c29cfdeff
Daze <err.liang@gmail.com>
2019-07-11 19:13:58 -0400

    Code cleanup

--- PythonChessBot/bot.py
+++ PythonChessBot/bot.py
@@ -1,7 +1,9 @@
 import time
-import chess.uci
+
+import chess.engine
 from selenium import webdriver
 from selenium.common.exceptions import StaleElementReferenceException
+
 from constants import *
 from selenium_chess import SeleniumChess
 
@@ -14,17 +16,16 @@ class Bot:
         self.engine_scores = []
         self.player = None
         self.board = None
+        self.scrape_count = 0
 
         self.driver = webdriver.Firefox()
-        self.engine = chess.uci.popen_engine(ENGINE_RELATIVE_DIRECTORY + '/' + ENGINE_NAME)
-        self.handler = chess.uci.InfoHandler()
+        self.engine = chess.engine.SimpleEngine.popen_uci(ENGINE_RELATIVE_DIRECTORY + '/' + ENGINE_NAME)
         self.interface = SeleniumChess(self.driver)
 
-        self.setup_chess()
         self.setup_browser()
         self.setup_selenium_chess()
 
-    def game_end(move):
+    def game_end(self, move):
         return move == '1-0' or move == '0-1' or move == '1/2-1/2'
 
     def login(self, username, password):
@@ -35,22 +36,14 @@ class Bot:
         self.driver.find_element_by_name("login").click()
 
     def can_play(self):
-        return len(self.move_list) > 0 and not Bot.game_end(self.move_list[-1]) or len(
-            self.move_list) == 0 and self.player == Side.WHITE
+        return (len(self.move_list) > 0 and not self.game_end(self.move_list[-1])) or (len(
+            self.move_list) == 0 and self.player == Side.WHITE)
 
     def setup_browser(self):
         self.driver.maximize_window()
         self.driver.get(START_URL)
         self.login(USERNAME, PASSWORD)
         self.driver.get(PLAY_CHESS_URL)
-
-    def setup_chess(self):
-        self.engine.uci()
-        if USING_MULTIPV:
-            self.engine.setoption({'MultiPV': MULTIPV_MOVE_COUNT + 1})
-
-        self.handler.multipv(MULTIPV_MOVE_COUNT)
-        self.engine.info_handlers.append(self.handler)
 
     def setup_selenium_chess(self):
         self.cvs_ctx.append(('first_cvs', 'first_ctx'))
@@ -59,7 +52,7 @@ class Bot:
     def run(self):
         while True:
             try:
-                time.sleep(1)
+                time.sleep(0.25)
 
                 start_time = time.time()
 
@@ -83,8 +76,6 @@ class Bot:
                     self.move_list = []  # rebuild move list if there's an error
                     continue
 
-                print(self.board)
-
                 if self.board.turn:
                     turn = Side.WHITE
                     turn_name = "WHITE"
@@ -107,12 +98,14 @@ class Bot:
                 print("Stale elements. Retrying...")
 
     def scrape_move_list(self):
-        if len(self.move_list) > 0:
+        if len(self.move_list) > 0 and self.scrape_count <= FULL_SCRAPE_LIMIT:
             last_move = self.interface.get_selected_move()  # by default the latest move is selected
             if last_move != self.move_list[-1]:
-                self.move_list = self.interface.get_move_list()
+                self.move_list.append(last_move)
         else:
             self.move_list = self.interface.get_move_list()
+            self.scrape_count = 0
+        self.scrape_count += 1
 
     def make_board(self):
         self.board = chess.Board(START_POS_FEN)
@@ -125,27 +118,32 @@ class Bot:
             print(self.board)
 
     def engine_eval(self):
-        self.engine.position(self.board)
-        evaluation = self.engine.go(depth=ENGINE_SEARCH_DEPTH)
-        self.engine_moves = [str(evaluation[0])]
-        self.engine_scores = [str(self.handler.info['score'][1].cp)]
-        for i in range(MULTIPV_MOVE_COUNT):
-            move_i, score_i = str(self.handler.info['pv'][i][0]), self.handler.info['score'][i].cp
-            self.engine_moves.append(move_i)
-            self.engine_scores.append(score_i)
+        if USING_MULTIPV:
+            evaluations = self.engine.analyse(board=self.board, limit=ENGINE_LIMIT, multipv=MULTIPV_MOVE_COUNT)
+            self.engine_moves = []
+            self.engine_scores = []
+            for i in range(len(evaluations)):
+                info = evaluations[i]
+                move_i, score_i = str(info['pv'][0]), info['score']
+                self.engine_moves.append(move_i)
+                self.engine_scores.append(score_i)
+        else:
+            evaluation = self.engine.play(board=self.board, limit=ENGINE_LIMIT)
+            self.engine_moves = [evaluation.move]
+            self.engine_scores = [str(evaluation.info['score'])]
 
     def display_moves(self):
         main_ctx_name = self.cvs_ctx[0][1]
         self.interface.graphics.clear_context(main_ctx_name)
         if USING_MULTIPV:
             if DRAW_TYPE == 'square':
-                for i in range(MULTIPV_MOVE_COUNT):
+                for i in range(len(self.engine_moves)):
                     self.interface.graphics.set_styles(main_ctx_name,
                                                        fillStyle=MULTIPV_MOVE_COLOURS[i - MULTIPV_MOVE_COUNT])
                     self.interface.draw_move_squares(main_ctx_name, self.engine_moves[i], self.player)
             elif DRAW_TYPE == 'arrow':
-                for i in range(MULTIPV_MOVE_COUNT):
-                    alpha = i * MULTIPV_ALPHA_STEP
+                for i in range(len(self.engine_moves)):
+                    alpha = (MULTIPV_MOVE_COUNT - i) * MULTIPV_ALPHA_STEP
                     self.interface.graphics.set_styles(main_ctx_name,
                                                        globalAlpha=str(alpha))
                     self.interface.draw_move_arrows(main_ctx_name, self.engine_moves[i], self.player)
